27eb821ad1f4270c693daf905d416967ca199642
404rorre <404rorre.edoc@gmail.com>
2022-09-27 19:37:05 +0200

    refactored code to functions

--- dayz_copying_mods.py
+++ dayz_copying_mods.py
@@ -6,46 +6,106 @@ def get_item_names(path):
 	"""Simple function to see items inside the folder."""
 	return os.listdir(path)
 
-file_name = "dayz_path.json"
+def universal_path_from_input(msg_helper):
+	"""
+	Simple function to make a clear path usable for linux and windows cmd
+	after input of request.
+	"""
+	return os.path.abspath(input(msg_helper))
 
-try:
+def get_json_file(file_name):
+	"""Function to search for save file and create a new one when not existing."""
 	with open(file_name) as f:
-		dayz_path = json.load(f)
-except FileNotFoundError:
-	#Get DayZ path
-	path_origin = input("Paste 'Dayz Addon'-Folder here:")
-	#Specify DayZ Server path
-	dayZ_server_folder = input("Paste 'Dayz Server'-Folder here:")
+		return json.load(f)
 
-	path_origin = os.path.abspath(path_origin)#"Y:/Gaming/1 Steam/steamapps/common/DayZ/!dzsal"
-	dayZ_server_folder = os.path.abspath(dayZ_server_folder)
+def get_dayz_folder_paths(file_name):
+	try:	
+		dayz_path = get_json_file(file_name)
+	except FileNotFoundError:
+		dayz_path = dump_json_dict(
+		file_name,
+		dayz_mod_folder=universal_path_from_input(
+										"Paste 'Dayz Addon'-Folder here:"),
+		dayZ_server_folder=universal_path_from_input(
+										"Paste 'Dayz Server'-Folder here:"),
+			)
+	return dayz_path["dayz_mod_folder"], dayz_path["dayZ_server_folder"]
 
-	dayz_path = {
-		"path_origin" : path_origin,
-		"dayZ_server_folder" : dayZ_server_folder,
-		}
-
+def dump_json_dict(file_name, **kwargs):
+	"""
+	Simple function to create dictionary and dump files as json file.
+	"""
 	with open(file_name, "w", encoding="utf-8") as f:
-		json.dump(dayz_path, f, indent=4)
+		json.dump(kwargs, f, indent=4)
+	return kwargs
 
-# path_origin = os.path.abspath(path_origin)#"Y:/Gaming/1 Steam/steamapps/common/DayZ/!dzsal"
-# dayZ_server_folder = os.path.abspath(dayZ_server_folder)#"Y:/Gaming/1 Steam/steamapps/common/DayZServer"
-new_path_addons = f"{dayz_path['dayZ_server_folder']}/copy_addons_here"
-new_path_keys = f"{dayz_path['dayZ_server_folder']}/copy_keys_here"
+def dump_modlist_for_bat(mods_for_bat):
+	"""
+	Simple function which dumps list into text file.
+	Can be copied into start.bat to start with special mods for server init.
+	"""
+	with open("mods_for_start_bat.txt", "w") as f:
+		f.write(mods_for_bat)
 
-bat_path = f"{dayz_path['dayZ_server_folder']}/mods_for_start_bat.txt"
+def report_fail(mod, reason):
+	"""Simple mod to create dictionary about reasons why copying failed."""
+	copy_fail = {
+				"mod" : mod,
+				"reason" : "no key folder"
+				}
+	copy_fails.append(copy_fail)
 
-mod_folders = get_item_names(dayz_path['path_origin'])
-print(f"{len(mod_folders)} mod-folders found.")
-raise_confirmation = input("Do you want to start copying the data to the DayZServer-folder? y/n")
-raise_confirmation = str(raise_confirmation)
+def copy_items_list(path_copy_from, path_extension, path_copy_to):
+	"""Simple function to copy items from one folder to another."""
+	items_folder = f"{path_copy_from}/{path_extension}"
+	items = get_item_names(items_folder)
+	for item in items:
+		shutil.copy(f"{items_folder}/{item}", path_copy_to, 
+			follow_symlinks=True)
+
+def copy_folders(path_copy_from, path_extension, path_copy_to):
+	"""Simple function to copy folders to another location."""
+	try:
+		shutil.copytree(path_copy_from, f"{path_copy_to}/{path_extension}")
+	except FileExistsError:
+		shutil.rmtree(f"{path_copy_to}/{path_extension}")
+		shutil.copytree(path_copy_from, f"{path_copy_to}/{path_extension}")
+		new_mod_folders.append(path_extension)
+
+def copy_mod_keys(mod_folder, new_path_keys):
+	"""
+	Simple function to test for different naming of key folders
+	and creating warning if not existing.
+	"""
+	try:
+		copy_items_list(mod_folder, "keys", new_path_keys)
+		return True			
+	except FileNotFoundError:
+		try:
+			copy_items_list(mod_folder, "key", new_path_keys)
+			return True
+		except FileNotFoundError:
+			report_fail(mod, "no key folder")
+			print(f"failed\n")
+			return False
+
+
+file_name = "dayz_path.json"
+dayz_mod_folder, dayZ_server_folder = get_dayz_folder_paths(file_name)
+new_path_addons = f"{dayZ_server_folder}/copy_addons_here"
+new_path_keys = f"{dayZ_server_folder}/copy_keys_here"
+
+#gets list of all mods inside folder
+mods = get_item_names(dayz_mod_folder)
+print(f"{len(mods)} mod-folders found.")
+raise_confirmation = str(input(
+	"Do you want to start copying the data to the DayZServer-folder? y/n"))
 
 mods_for_bat = ""
-failed_copies = []
+copy_fails = []
 new_mod_folders = []
 
 if raise_confirmation == "y":
-
 	#check if copy_folders exist if not make some
 	if not os.path.isdir(new_path_addons):
 		os.makedirs(new_path_addons)
@@ -53,81 +113,42 @@ if raise_confirmation == "y":
 		os.makedirs(new_path_keys)					
 
 	#copies each folder
-	for mod in mod_folders:
+	for mod in mods:
 		bat_flag = False
-		mod_folder = f"{dayz_path['path_origin']}/{mod}"
+		mod_folder = f"{dayz_mod_folder}/{mod}"
 		
+		#check if folder has items inside
 		item_inside_mod_flag = os.path.isdir(mod_folder)
-
 		#create a string to add to batch file to start with server
 		print(f"copying {mod} | Items: {item_inside_mod_flag}...", end="")
-
-		#check if folder has items inside
-		if item_inside_mod_flag:
-			bat_flag = True			
+		#start copying
+		if item_inside_mod_flag:			
 			#copying mod folder to DayZ Server folder
-			try:
-				shutil.copytree(f"{mod_folder}", f"{dayz_path['dayZ_server_folder']}/{mod}")
-			except FileExistsError:
-				shutil.rmtree(f"{dayz_path['dayZ_server_folder']}/{mod}")
-				shutil.copytree(f"{mod_folder}", f"{dayz_path['dayZ_server_folder']}/{mod}")
-				new_mod_folders.append(mod)
-
+			copy_folders(mod_folder, mod, dayZ_server_folder)
 			#copying the addons of the mod
-			addon_folder = f"{mod_folder}/addons"
-			addons = get_item_names(addon_folder)
-			for addon in addons:
-				shutil.copy(f"{addon_folder}/{addon}", new_path_addons, follow_symlinks=True)
-
+			copy_items_list(mod_folder, "addons", new_path_addons)			
 			#copying the keys of the mod
-			try:
-				key_folder = f"{mod_folder}/keys"
-				keys = get_item_names(key_folder)
-				for key in keys:
-					shutil.copy(f"{key_folder}/{key}", new_path_keys, follow_symlinks=True)
-			except FileNotFoundError:
-				try:
-					key_folder = f"{mod_folder}/key"
-					keys = get_item_names(key_folder)
-					for key in keys:
-						shutil.copy(f"{key_folder}/{key}", new_path_keys, follow_symlinks=True)
-
-				except FileNotFoundError:
-					fail_copy = {
-						"mod" : mod,
-						"reason" : "no key folder"
-						}
-					failed_copies.append(fail_copy)
-					print(f"failed\n")
-					bat_flag = False
-
+			bat_flag = copy_mod_keys(mod_folder, new_path_keys)
+			#continues copying if bat_flag True (copying successfull)
 			if bat_flag:
 				#mod string fot "start.bat"
 				mods_for_bat += f"{mod};"
-
 				print(f"successfull\n")
-
 		else:
-			fail_copy = {
-					"mod" : mod,
-					"reason" : "no items found inside mod folder"
-					}
-			failed_copies.append(fail_copy)
-
+			report_fail(mod, "no items found inside mod folder")
 			print(f"failed\n")
-
 else:
 	print("copy process aborted")
 
+
 print("Following mods could not be copied:")
-for fail_copy in failed_copies:
-		print(f"Mod: {fail_copy['mod']}\tReason: {fail_copy['reason']}")
+for copy_fail in copy_fails:
+		print(f"Mod: {copy_fail['mod']}\tReason: {copy_fail['reason']}")
 
 print("Following Mods have been updated:")
 for mod in new_mod_folders:
 		print(mod)
 
-with open(bat_path, "w") as f:
-	f.write(mods_for_bat)
+dump_modlist_for_bat(mods_for_bat)
 
 input("Continue with Enter")
